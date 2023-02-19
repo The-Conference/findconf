c5d723892397770359085a0ec74cb788987e93d1
kirill <kirill@Kirillsucker.localdomain>
2023-02-19 18:07:03 +0300

    API serializer corrected

--- Conference_data/serializers.py
+++ Conference_data/serializers.py
@@ -5,7 +5,7 @@ from .models import Conference
 class ConferenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conference
-        fields = ('conf_id', 'hash', 'un_name', 'local',
+        fields = ('id', 'conf_id', 'hash', 'un_name', 'local',
                   'reg_date_begin', 'reg_date_end', 'conf_date_begin',
                   'conf_date_end', 'conf_card_href', 'reg_href',
                   'conf_name', 'conf_s_desc', 'conf_desc', 'org_name',
