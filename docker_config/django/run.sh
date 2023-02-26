05aa0f8ca5d3073be455ecf90f48e0f1aef82624
SergeyErshov <s.ershov.shoko@gmail.com>
2023-02-26 13:43:42 +0300

    add autocreate superuser for django app

--- docker_config/django/run.sh
+++ docker_config/django/run.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-sleep 15
+sleep 20
 
+source /app/Conferences/.env
+export DJANGO_SUPERUSER_PASSWORD=$(echo $DJANGO_PASS)
+export DJANGO_SUPERUSER_EMAIL="admin@email.no"
 python manage.py makemigrations
 python manage.py migrate
+python manage.py createsuperuser --username $(echo $DJANGO_USER) --no-input
 #python manage.py createcachetable
 #python manage.py collectstatic  --noinput
 gunicorn Conferences.wsgi:application --bind 0.0.0.0:8000
