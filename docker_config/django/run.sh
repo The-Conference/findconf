6e1526b18d2a13de0ca76f25ee1b40232d483326
SergeyErshov <s.ershov.shoko@gmail.com>
2023-03-02 12:14:33 +0300

    createsuperuser with env (--no-input) options

--- docker_config/django/run.sh
+++ docker_config/django/run.sh
@@ -3,13 +3,11 @@
 sleep 20
 
 source /app/Conferences/.env
-export DJANGO_SUPERUSER_PASSWORD=$(echo $DJANGO_PASS)
-export DJANGO_SUPERUSER_EMAIL="admin@email.no"
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --username $(echo $DJANGO_USER) --no-input
-#python manage.py createcachetable
-#python manage.py collectstatic  --noinput
+python manage.py createsuperuser --no-input
+python manage.py createcachetable
+python manage.py collectstatic  --noinput
 gunicorn Conferences.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
