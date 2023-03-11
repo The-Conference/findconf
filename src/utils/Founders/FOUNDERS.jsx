abb9a4f3fe42f439989961c4e851594e7e832c8f
Andrew <malinaenglishclub@gmail.com>
2023-03-11 17:14:50 +0300

    build update

--- src/utils/Founders/FOUNDERS.jsx
+++ src/utils/Founders/FOUNDERS.jsx
@@ -4,6 +4,7 @@ import Mosh from "../Founders/pics/Mosh.png";
 import Ershov from "../Founders/pics/Ershov.png";
 import Fromm from "../Founders/pics/Fromm.png";
 import Novikov from "../Founders/pics/Novikov.png";
+import Makover from "../Founders/pics/Makover.png";
 
 const founders = [
   { role: "Project Manager", name: "Иван Иванов", pic: profPic },
@@ -17,6 +18,7 @@ const developers = [
   { role: "Backend", name: "Кирилл Левин", pic: Levin },
   { role: "DevOps", name: "Сергей Ершов", pic: Ershov },
   { role: "Backend", name: "Вячеслав Новиков", pic: Novikov },
+  { role: "Backend", name: "Андрей Маковер", pic: Makover },
   { role: "Frontend", name: "Андрей Мощенко", pic: Mosh },
 ];
 
