2f2efac5ad10cb329a7111edec8bb71cbfdb99a9
Andrew <malinaenglishclub@gmail.com>
2023-03-16 14:54:17 +0300

    build update

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://test.theconf.ru:8000/",
+  baseURL: "https://test.theconf.ru/",
   headers: {
     "Content-Type": "application/json",
   },
