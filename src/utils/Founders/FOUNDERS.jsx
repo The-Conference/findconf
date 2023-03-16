bd1c7569505f718d43b1c1f62a7fb1d2757f8763
Andrew <malinaenglishclub@gmail.com>
2023-03-16 21:41:33 +0300

    card animation, some minor style bugs fixed

--- src/utils/Founders/FOUNDERS.jsx
+++ src/utils/Founders/FOUNDERS.jsx
@@ -5,7 +5,7 @@ import Ershov from "../Founders/pics/Ershov.webp";
 import Fromm from "../Founders/pics/Fromm.webp";
 import Novikov from "../Founders/pics/Novikov.webp";
 import Makover from "../Founders/pics/Makover.webp";
-
+import Vorobiova from "../Founders/pics/Vorobiova.webp";
 const founders = [
   { role: "Project Manager", name: "Иван Иванов", pic: profPic },
   { role: "Project Manager", name: "Иван Иванов", pic: profPic },
@@ -15,10 +15,12 @@ const founders = [
 
 const developers = [
   { role: "UX/UI", name: "Марк Fromm", pic: Fromm },
+  { role: "Graphic design", name: "Дарья Воробьева", pic: Vorobiova },
   { role: "Backend", name: "Кирилл Левин", pic: Levin },
   { role: "DevOps", name: "Сергей Ершов", pic: Ershov },
   { role: "Backend", name: "Вячеслав Новиков", pic: Novikov },
   { role: "Backend", name: "Андрей Маковер", pic: Makover },
+
   { role: "Frontend", name: "Андрей Мощенко", pic: Mosh },
 ];
 
