db553d66f64bc16dadb044465545751b609f8b28
Andrew <malinaenglishclub@gmail.com>
2023-03-16 16:53:52 +0300

    new build for prod

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-<<<<<<< HEAD
   baseURL: "https://theconf.ru/",
-=======
-  baseURL: "https://test.theconf.ru/",
->>>>>>> frontend
   headers: {
     "Content-Type": "application/json",
   },
