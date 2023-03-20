55cf4221d3a81668d61fc06fecbe565926e91470
SergeyErshov <s.ershov.shoko@gmail.com>
2023-03-20 18:34:31 +0300

    add checks for temp dir exists, rename it to download

--- scripts-test/front_update.sh
+++ scripts-test/front_update.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-cd /tmp/distrib && wget https://github.com/The-Conference/findconf/archive/refs/heads/frontend.zip
-unzip /tmp/distrib/frontend.zip
+temp_dir="/tmp/download"
+
+if [ ! -d $temp_dir ]; then
+    mkdir $temp_dir
+fi
+
+cd $temp_dir && wget https://github.com/The-Conference/findconf/archive/refs/heads/frontend.zip
+unzip $temp_dir/frontend.zip
 rm -rf /usr/data/www/*
-cp -R /tmp/distrib/findconf-frontend/build/* /usr/data/www
-rm -rf /tmp/distrib/findconf-frontend /tmp/distrib/frontend.zip
+cp -R $temp_dir/findconf-frontend/build/* /usr/data/www
+rm -rf $temp_dir/findconf-frontend $temp_dir/frontend.zip
