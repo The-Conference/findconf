8d04ce580b222f5218e445c767f1011737248ba7
Andrew <malinaenglishclub@gmail.com>
2023-03-20 12:56:34 +0300

    build update

--- src/utils/Founders/FOUNDERS.jsx
+++ src/utils/Founders/FOUNDERS.jsx
@@ -8,7 +8,10 @@ import Vorobiova from "../Founders/pics/Vorobiova.webp";
 import Rubaeva from "../Founders/pics/Rubaeva.webp";
 import Semenova from "../Founders/pics/Semenova.webp";
 import Krainov from "../Founders/pics/Krainov.webp";
+import Peregudov from "../Founders/pics/Peregudov.webp";
+import Ivanova from "../Founders/pics/Ivanova.webp";
 const founders = [
+  { role: "Project Manager", name: "Сергей Перегудов", pic: Peregudov },
   { role: "Project Manager", name: "Татьяна Семёнова", pic: Semenova },
   { role: "Project Mentor", name: "Кирилл Крайнов", pic: Krainov },
 ];
@@ -22,6 +25,7 @@ const developers = [
   { role: "Backend", name: "Андрей Маковер", pic: Makover },
   { role: "Content Manager", name: "Анастасия Рубаева", pic: Rubaeva },
   { role: "Frontend", name: "Андрей Мощенко", pic: Mosh },
+  { role: "QA Tester", name: "Юлия Иванова", pic: Ivanova },
 ];
 
 export { founders, developers };
