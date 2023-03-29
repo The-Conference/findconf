717402a9ec10453cf3fb7102b13caf573e4bfb73
kirill <kirill@Kirillsucker.localdomain>
2023-03-29 21:24:59 +0300

    Corrected Celery schedule for prod

--- Conferences/celery.py
+++ Conferences/celery.py
@@ -13,6 +13,6 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'running_parsers': {
         'task': 'Conference_data.tasks.parser1',
-        'schedule': crontab(minute=0, hour=11, day_of_week='thu'),
+        'schedule': crontab(minute=35, hour=18, day_of_week='wed'),
     },
 }
