93e94e254baca5789927056d676f65d949a6d12f
SergeyErshov <s.ershov.shoko@gmail.com>
2023-05-03 14:44:49 +0300

    add making backup before update

--- scripts-test/front_update.sh
+++ scripts-test/front_update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 temp_dir="/tmp/download"
+now=$(date +'%Y-%m-%dT%H-%M-%S')
 
 if [ ! -d $temp_dir ]; then
     mkdir $temp_dir
 fi
+
+cp -R /usr/data/www /usr/data/www-$now-man
 
 cd $temp_dir && wget https://github.com/The-Conference/findconf/archive/refs/heads/frontend.zip
 unzip $temp_dir/frontend.zip
