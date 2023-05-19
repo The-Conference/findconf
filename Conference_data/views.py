4bbf157fc666ec8c97a2567f94511fd028e26f0b
kirill <kirill@Kirillsucker.localdomain>
2023-05-19 17:23:54 +0300

    API filters (online + offline) added

--- Conference_data/views.py
+++ Conference_data/views.py
@@ -9,7 +9,18 @@ class ConferenceList(generics.ListCreateAPIView):
     serializer_class = ConferenceSerializer
 
     def get_queryset(self):
-        return Conference.objects.filter(checked=True)
+        offline = self.request.query_params.get('offline', None)
+        online = self.request.query_params.get('online', None)
+
+        queryset = Conference.objects.filter(checked=True)
+
+        if offline is not None:
+            queryset = queryset.filter(offline=bool(offline))
+
+        if online is not None:
+            queryset = queryset.filter(online=bool(online))
+
+        return queryset
 
 
 class ConferenceDetail(generics.RetrieveUpdateDestroyAPIView):
