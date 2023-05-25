f650b36933bb496649e022e42f2ea23b00dea684
Den <d.zapekin@gmail.com>
2023-05-25 13:15:45 +0200

    Conference_crm fix urls, settings

--- Conference_crm/urls.py
+++ Conference_crm/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import UserByToken
 
 urlpatterns = [
-    path('user/by/token/', UserByToken.as_view()),
+    path('by/token/', UserByToken.as_view()),
 ]
--- Conferences/urls.py
+++ Conferences/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
     path('api/conf_auth/', include('rest_framework.urls')),
     path('api/auth/', include('djoser.urls')),
     path('api/auth-token/', include('djoser.urls.authtoken')),
-    # path('api/user/', include('Conference_crm.urls')),
+    path('api/user/', include('Conference_crm.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
