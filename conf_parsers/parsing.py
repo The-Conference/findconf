fc5d2b0036ae19f7b7ec214c95c7123f9b0dd302
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-06-06 01:27:50 +0300

    xpath generic parser

--- conf_parsers/parsing.py
+++ conf_parsers/parsing.py
@@ -1,38 +1,26 @@
+"""Common parsing logic shared by most parsers.
+
+Intended for use with any given website, with the most abstract rules so that they apply anywhere.
+More accurate, site-specific rules can be added to spiders on top of these functions.
+"""
+
 import re
 from bs4 import Tag
+from scrapy import Selector
 from scrapy.loader import ItemLoader
 from .utils import find_date_in_string, parse_vague_dates
 
 
 def default_parser_bs(line: Tag, new_item: ItemLoader) -> ItemLoader:
-    lowercase = line.text.lower()
-
-    new_item.add_value('conf_desc', line.get_text(separator=" "))
-    if 'ринц' in lowercase:
-        new_item.add_value('rinc', True)
-    if 'scopus' in lowercase:
-        new_item.add_value('scopus', True)
-    if 'ВАК' in line.text:
-        new_item.add_value('vak', True)
-    if 'wos' in lowercase:
-        new_item.add_value('wos', True)
-
-    if ('состоится' in lowercase
-            or 'открытие' in lowercase
-            or 'проведен' in lowercase
-            or 'дата' in lowercase
-            or 'пройд' in lowercase
-            or 'проход' in lowercase
-            or 'провод' in lowercase):
-        new_item = get_dates(lowercase, new_item)
+    """Deprecation warning: this function will be replaced with xpath selectors below."""
+    lowercase = line.text.casefold()
 
     if ('заявк' in lowercase
             or 'принимаютс' in lowercase
             or 'участия' in lowercase
             or 'регистр' in lowercase):
         if dates := find_date_in_string(lowercase):
-            if ('до' in lowercase or 'оконч' in lowercase or 'срок' in lowercase)\
-                    and len(dates) == 1:
+            if ('до' in lowercase or 'оконч' in lowercase or 'срок' in lowercase) and len(dates) == 1:
                 new_item.add_value('reg_date_end', dates[0])
             else:
                 new_item.add_value('reg_date_begin', dates[0])
@@ -48,9 +36,6 @@ def default_parser_bs(line: Tag, new_item: ItemLoader) -> ItemLoader:
         except TypeError:
             pass
 
-    if 'организатор' in lowercase:
-        new_item.add_value('org_name', line.get_text(separator=" "))
-
     if ('онлайн' in lowercase
             or 'трансляц' in lowercase
             or 'гибридн' in lowercase
@@ -59,37 +44,117 @@ def default_parser_bs(line: Tag, new_item: ItemLoader) -> ItemLoader:
         new_item.add_value('conf_href', line.find('a').get('href') if line.find('a') else None)
         new_item.add_value('online', True)
 
+    return parse_plain_text(new_item, line.get_text(separator=' '), lowercase)
+
+
+def default_parser_xpath(selector: Selector, new_item: ItemLoader) -> ItemLoader:
+    """Main starting point for generic data parsing & collection.
+
+    Args:
+        selector (Selector): A selector item with text and tags, as it comes from the response.
+        new_item (ItemLoader): ItemLoader object to append discovered data to.
+
+    Returns:
+        Populated ItemLoader object.
+    """
+    line_text = selector.xpath("string(.)").get()
+    lowercase = line_text.casefold()
+
+    if ('заявк' in lowercase
+            or 'принимаютс' in lowercase
+            or 'участия' in lowercase
+            or 'регистр' in lowercase):
+        if dates := find_date_in_string(lowercase):
+            if ('до' in lowercase or 'оконч' in lowercase or 'срок' in lowercase) and len(dates) == 1:
+                new_item.add_value('reg_date_end', dates[0])
+            else:
+                new_item.add_value('reg_date_begin', dates[0])
+                new_item.add_value('reg_date_end', dates[1] if 1 < len(dates) else None)
+
+        link = selector.xpath(".//a/@href").get()
+        if link and ('.pdf' not in link or '.doc' not in link or '.xls' not in link):
+            new_item.add_value('reg_href', link)
+
+    if ('онлайн' in lowercase
+            or 'трансляц' in lowercase
+            or 'гибридн' in lowercase
+            or 'дистанц' in lowercase
+            or 'ссылка' in lowercase):
+        new_item.add_value('conf_href', selector.xpath(".//a/@href").get())
+        new_item.add_value('online', True)
+
+    return parse_plain_text(new_item, line_text, lowercase)
+
+
+def parse_plain_text(new_item: ItemLoader, line: str, lowercase: str = None) -> ItemLoader:
+    """Search plain text for various data markers, parse, and populate the supplied ItemLoader object
+    with the results. Append every supplied line of text to the description field.
+
+    Args:
+        line: Text to parse, stripped from tags, normalization is not required (handled by the loader).
+        lowercase: Same text, casefolded for comparisons.
+        new_item: ItemLoader object to append discovered data to.
+
+    Returns:
+        Populated ItemLoader object.
+    """
+    if lowercase is None:
+        lowercase = line.casefold()
+
+    new_item.add_value('conf_desc', line)
+
+    if 'ринц' in lowercase:
+        new_item.add_value('rinc', True)
+    if 'scopus' in lowercase:
+        new_item.add_value('scopus', True)
+    if 'ВАК' in line:
+        new_item.add_value('vak', True)
+    if 'wos' in lowercase:
+        new_item.add_value('wos', True)
+
+    if ('состоится' in lowercase
+            or 'открытие' in lowercase
+            or 'проведен' in lowercase
+            or 'дата' in lowercase
+            or 'пройд' in lowercase
+            or 'проход' in lowercase
+            or 'провод' in lowercase):
+        new_item = get_dates(lowercase, new_item)
+
+    if emails := re.search(r'\S+@\S+\.\S+', lowercase):
+        new_item.add_value('contacts', emails.group(0))
+
+    if 'организатор' in lowercase:
+        new_item.add_value('org_name', line)
+
     if ('место' in lowercase
             or 'адрес' in lowercase
             or 'город' in lowercase
             or 'очно' in lowercase):
-        new_item.add_value('conf_address', line.text)
+        new_item.add_value('conf_address', line)
         new_item.add_value('offline', True)
 
     if ('тел.' in lowercase
             or 'контакт' in lowercase
             or 'mail' in lowercase
             or 'почт' in lowercase):
-        new_item.add_value('contacts', line.text)
-
-    if emails := re.search(r'\S+@\S+\.\S+', lowercase):
-        new_item.add_value('contacts', emails.group(0))
+        new_item.add_value('contacts', line)
 
     return new_item
 
 
 def get_dates(string: str, new_item: ItemLoader, is_vague: bool = False) -> ItemLoader:
-    """
-    Search a string for dates; convert them to datetime format
+    """Search a string for dates, convert them to datetime format,
     and append to the supplied ItemLoader object.
     See :class:`tests<conf_spiders.tests.test_utils.TestDateFinder>` for a list of supported date formats.
+    For a more general solution, see :func:`~conf_spiders.utils.find_date_in_string`.
 
     Args:
         string: A string that may or may not contain dates.
         new_item: An ItemLoader object to write dates to.
         is_vague: Use this option to handle uncertain dates, e.g. just month.
             Warning: use this only if the string is certain to contain a date,
-            otherwise false positives and/or unforseen errors may occur.
+            otherwise false positives and/or unforeseen errors may occur.
 
     Returns:
         ItemLoader object with dates appended (if found).
--- conf_parsers/spiders/mgppu.py
+++ conf_parsers/spiders/mgppu.py
@@ -1,8 +1,7 @@
 from scrapy.spiders import Rule, CrawlSpider
-from bs4 import BeautifulSoup
 from scrapy.linkextractors import LinkExtractor
 from ..items import ConferenceItem, ConferenceLoader
-from ..parsing import default_parser_bs, get_dates
+from ..parsing import default_parser_xpath, get_dates
 
 
 class MgppuSpider(CrawlSpider):
@@ -24,9 +23,6 @@ class MgppuSpider(CrawlSpider):
         new_item.add_value('conf_name', conf_name)
         new_item.add_value('conf_s_desc', conf_name)
 
-        soup = BeautifulSoup(response.text, 'lxml')
-        conf_block = soup.find('div', class_='page container').find('div', class_='col-sm-12')
-        lines = conf_block.find_all(['p', 'ul', 'ol'])
-        for line in lines:
-            new_item = default_parser_bs(line, new_item)
+        for line in response.xpath("//article/*[self::p or self::ul]"):
+            new_item = default_parser_xpath(line, new_item)
         yield new_item.load_item()
