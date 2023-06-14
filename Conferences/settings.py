16e0a9b6b35a56a3007c756703ace7e5f4795ea5
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-06-14 16:56:44 +0300

    Remove Celery

--- Conferences/settings.py
+++ Conferences/settings.py
@@ -170,19 +170,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# celery
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-
-# CELERY_BROKER_URL = 'redis://redis:6379'
-# CELERY_RESULT_BACKEND = 'redis://redis:6379'
-# CELERY_ACCEPT_CONTENT = ['application/json']
-# CELERY_TASK_SERIALIZER = 'json'
-# CELERY_RESULT_SERIALIZER = 'json'
-
 # ckEditor config
 CKEDITOR_CONFIGS = {
     'default': {
