55bf6e03597fd053aac0902ffc1ecce23d06431d
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-06-18 16:46:30 +0300

    tsu

---
+++ conf_parsers/spiders/tsu.py
@@ -0,0 +1,30 @@
+import scrapy
+from ..items import ConferenceItem, ConferenceLoader
+from ..parsing import default_parser_xpath, get_dates
+
+
+class TsuSpider(scrapy.Spider):
+    name = "tsu"
+    un_name = 'Национальный исследовательский Томский государственный университет'
+    allowed_domains = ["tsu.ru"]
+    start_urls = ["https://news.tsu.ru/calendar-of-events/rss.php"]
+
+    def parse(self, response, **kwargs):
+        for item in response.xpath('//channel/item'):
+            title = item.xpath('title//text()').get()
+            link = item.xpath('link//text()').get()
+            if 'онференц' in title:
+                yield scrapy.Request(link, callback=self.parse_items)
+
+    def parse_items(self, response):
+        new_item = ConferenceLoader(item=ConferenceItem(), selector=response)
+
+        new_item.add_value('conf_card_href', response.url)
+        new_item.add_css('conf_name', "h1::text")
+
+        for line in response.xpath("//div[@class='text-content']/*[self::p or self::ul or self::ol]"):
+            new_item = default_parser_xpath(line, new_item)
+        if not new_item.get_collected_values('conf_date_begin'):
+            new_item = get_dates(response.xpath("string(//div[@class='address'])").get(), new_item)
+
+        yield new_item.load_item()
