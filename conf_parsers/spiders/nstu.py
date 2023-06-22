aa1e1b2697a4f71f66ddad3369053dc2dee73f7e
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-06-22 17:25:32 +0300

    nstu

---
+++ conf_parsers/spiders/nstu.py
@@ -0,0 +1,28 @@
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+
+from ..items import ConferenceItem, ConferenceLoader
+from ..parsing import default_parser_xpath, get_dates
+
+
+class NstuSpider(CrawlSpider):
+    name = "nstu"
+    un_name = 'Новосибирский государственный технический университет'
+    allowed_domains = ["nstu.ru"]
+    start_urls = ["https://www.nstu.ru/science/scientific_events"]
+    rules = (
+        Rule(LinkExtractor(restrict_css="div.science-events__item-cell"),
+             callback="parse_items", follow=False),
+    )
+
+    def parse_items(self, response):
+        new_item = ConferenceLoader(item=ConferenceItem(), selector=response)
+
+        new_item.add_value('conf_card_href', response.url)
+        new_item.add_css('conf_name', "h3::text")
+        dates = response.xpath("string(//div[@class='text-bold mb-1'])").get()
+        new_item = get_dates(dates, new_item)
+
+        for line in response.xpath("//main[@class='page-content']/div/div[@class='row']"):
+            new_item = default_parser_xpath(line, new_item)
+        yield new_item.load_item()
