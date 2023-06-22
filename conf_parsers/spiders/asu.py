4e7aa309eadf53f79904f6e24e6458d71a7c716f
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-06-22 15:46:41 +0300

    asu

---
+++ conf_parsers/spiders/asu.py
@@ -0,0 +1,34 @@
+import scrapy
+
+from ..items import ConferenceItem, ConferenceLoader
+from ..parsing import default_parser_xpath, get_dates
+
+
+class AsuSpider(scrapy.Spider):
+    name = "asu"
+    un_name = 'Алтайский государственный университет'
+    allowed_domains = ["asu.ru"]
+    start_urls = ["https://www.asu.ru/science/sci_events/"]
+
+    def parse(self, response, **kwargs):
+        for row in response.xpath("//div[@class='tabl_events']//tr"):
+            r = [i.xpath("string(.)").get() for i in row.xpath(".//td")]
+            try:
+                conf_type = r[1]
+                title, dates, contacts = r[3:6]
+                orgs, desc1, desc2 = r[8:11]
+            except (IndexError, ValueError):
+                continue
+
+            if 'онференц' in conf_type.lower():
+                new_item = ConferenceLoader(item=ConferenceItem(), selector=response)
+
+                new_item.add_value('conf_card_href', response.url)
+                new_item.add_value('conf_name', title)
+                new_item = get_dates(dates, new_item, is_vague=True)
+                new_item = default_parser_xpath(desc1, new_item)
+                new_item = default_parser_xpath(desc2, new_item)
+                new_item.add_value('contacts', contacts)
+                new_item.add_value('org_name', orgs)
+
+                yield new_item.load_item()
