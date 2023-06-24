4f617d6a2e36e36ec2f3ad5b00d887b17339d3cf
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-06-24 23:32:19 +0300

    sechenov

---
+++ conf_parsers/spiders/sechenov.py
@@ -0,0 +1,25 @@
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+from ..items import ConferenceItem, ConferenceLoader
+from ..parsing import default_parser_xpath
+
+
+class SechenovSpider(CrawlSpider):
+    name = "sechenov"
+    un_name = 'Первый МГМУ им. И.М. Сеченова Минздрава России (Сеченовский Университет)'
+    allowed_domains = ["www.sechenov.ru"]
+    start_urls = ["https://www.sechenov.ru/pressroom/events/"]
+    rules = (
+        Rule(LinkExtractor(restrict_css='div.event-descr-wrap', restrict_text='онференц'),
+             callback="parse_items", follow=False),
+    )
+
+    def parse_items(self, response):
+        new_item = ConferenceLoader(item=ConferenceItem(), selector=response)
+
+        new_item.add_value('conf_card_href', response.url)
+        new_item.add_xpath('conf_name', "string(//div[@class='news-header'])")
+
+        for line in response.xpath("//div[@class='event-detail-txt']//*[self::p or self::li]"):
+            new_item = default_parser_xpath(line, new_item)
+        yield new_item.load_item()
