204a666fb861b422527c2bc2eda9aed24ae8825a
SergeyErshov <s.ershov.shoko@gmail.com>
2023-07-03 12:41:14 +0300

    change to 'drf_spectacular',

--- docker_config/django/settings.py
+++ docker_config/django/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_swagger',
+    'drf_spectacular',
     'corsheaders',
     'ckeditor',
     'Conference_data',
