6117d81e028cd8f452bb68569a2f47b72efbacb1
SergeyErshov <s.ershov.shoko@gmail.com>
2023-07-24 16:50:13 +0300

    setting.py - change staticfiles_dirs - 240723 Denis

--- docker_config/django/settings.py
+++ docker_config/django/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
-    ('static', '/usr/local/lib/python3.11/site-packages/django/contrib/admin'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
