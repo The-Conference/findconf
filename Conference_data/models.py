a3242e6f3191a4d4e8b3e259c19548fa04b4a5e8
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-08-22 12:49:42 +0300

    add grants model, abstract model

--- Conference_data/models.py
+++ Conference_data/models.py
@@ -19,45 +19,32 @@ class Tag(models.Model):
         return f'{self.name}'
 
 
-class Conference(models.Model):
+class AbstractItem(models.Model):
     conf_id = models.TextField(unique=True)
     hash = models.CharField(null=True, blank=True, max_length=500)  # Legacy code, deprecated
     un_name = models.CharField(max_length=500, verbose_name="ВУЗ")
     local = models.BooleanField(default=True, verbose_name="Международная")
     reg_date_begin = models.DateField(null=True, blank=True, verbose_name="Начало регистрации")
     reg_date_end = models.DateField(null=True, blank=True, verbose_name="Окончание регистрации")
-    conf_date_begin = models.DateField(verbose_name="Дата начала")
-    conf_date_end = models.DateField(null=True, blank=True, verbose_name="Дата окончания")
     conf_card_href = models.URLField(null=True, blank=True, max_length=500, verbose_name="Карточка конференции")
     reg_href = models.URLField(null=True, blank=True, max_length=500, verbose_name="Ссылка на регистрацию")
     conf_name = models.TextField(verbose_name="Название")
     conf_s_desc = RichTextField(null=True, blank=True, verbose_name="Краткое описание")
     conf_desc = RichTextField(verbose_name="Описание")
-    org_name = models.TextField(null=True, blank=True, verbose_name="Организатор")
-    themes = models.TextField(null=True, blank=True, verbose_name="Тематика (от организатора)")
-    online = models.BooleanField(default=False)
-    conf_href = models.URLField(null=True, blank=True, max_length=500, verbose_name="Ссылка")
-    offline = models.BooleanField(default=True)
-    conf_address = models.TextField(null=True, blank=True, verbose_name="Адрес")
     contacts = models.TextField(null=True, blank=True, verbose_name="Контакты")
-    rinc = models.BooleanField(default=False, verbose_name="РИНЦ")
-    data = models.JSONField(blank=True, null=True)  # Legacy code, deprecated
     checked = models.BooleanField(default=False, verbose_name="Проверено")
-    tags = models.ManyToManyField(Tag, blank=True, verbose_name="Теги")
 
-    generate_conf_id = models.BooleanField(default=False)  # Legacy code, deprecated
+    class Meta:
+        abstract = True
 
-    vak = models.BooleanField(default=False)
-    wos = models.BooleanField(default=False)
-    scopus = models.BooleanField(default=False)
-
-    favorites = GenericRelation('Favorite')
+    def __str__(self):
+        return f'{self.un_name} - {self.conf_name}'
 
     def clean(self) -> None:
         """Validate_unique is needed to show an error in admin,
         otherwise it fails with error 500."""
         if not self.conf_id:
-            self.conf_id = f"{self.un_name[:100]}{self.conf_name[:100]}{self.conf_date_begin}"
+            self.conf_id = f"{self.un_name[:100]}{self.conf_name[:100]}"
         self.validate_unique()
 
     def save(self, *args, **kwargs):
@@ -71,6 +58,30 @@ class Conference(models.Model):
         if string:
             string.replace('\n', '').replace('\t', '')
         return string
+
+
+class Conference(AbstractItem):
+    conf_date_begin = models.DateField(verbose_name="Дата начала")
+    conf_date_end = models.DateField(null=True, blank=True, verbose_name="Дата окончания")
+    org_name = models.TextField(null=True, blank=True, verbose_name="Организатор")
+    themes = models.TextField(null=True, blank=True, verbose_name="Тематика (от организатора)")
+    online = models.BooleanField(default=False, verbose_name="Онлайн")
+    conf_href = models.URLField(null=True, blank=True, max_length=500, verbose_name="Сайт конференции")
+    offline = models.BooleanField(default=True, verbose_name="Оффлайн")
+    conf_address = models.TextField(null=True, blank=True, verbose_name="Адрес")
+    rinc = models.BooleanField(default=False, verbose_name="РИНЦ")
+    data = models.JSONField(blank=True, null=True)  # Legacy code, deprecated
+    generate_conf_id = models.BooleanField(default=False)  # Legacy code, deprecated
+    vak = models.BooleanField(default=False, verbose_name="ВАК")
+    wos = models.BooleanField(default=False, verbose_name="WoS")
+    scopus = models.BooleanField(default=False, verbose_name="Scopus")
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name="Теги")
+    favorites = GenericRelation('Favorite')
+
+    def clean(self) -> None:
+        if not self.conf_id:
+            self.conf_id = f"{self.un_name[:100]}{self.conf_name[:100]}{self.conf_date_begin}"
+        self.validate_unique()
 
     @property
     def conf_status(self) -> str:
@@ -90,8 +101,14 @@ class Conference(models.Model):
         verbose_name = "Конференция"
         verbose_name_plural = "Конференции"
 
-    def __str__(self):
-        return f'{self.un_name} - {self.conf_name}'
+
+class Grant(AbstractItem):
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name="Теги")
+    favorites = GenericRelation('Favorite')
+
+    class Meta:
+        verbose_name = "Грант"
+        verbose_name_plural = "Гранты"
 
 
 class Favorite(models.Model):
