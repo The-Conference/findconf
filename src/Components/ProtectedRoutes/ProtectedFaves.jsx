476a9efa7e5e9ab6442e768773a8effc1c25b824
Andrew <malinaenglishclub@gmail.com>
2023-08-30 11:07:09 +0300

    protected route mobile fixed

--- src/Components/ProtectedRoutes/ProtectedFaves.jsx
+++ src/Components/ProtectedRoutes/ProtectedFaves.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import AllConferences from "../Conference/AllConferences";
 import Login from "../Login/Login";
@@ -6,18 +6,49 @@ import Header from "../Header/Header";
 // import Footer from "../Footer/Footer";
 import FloatingMenu from "../FloatingMenu/FloatingMenu";
 import SideBar from "../SideBar/SideBar";
+import useOnClickOutside from "../Hooks/useOnClickOutside";
 const ProtectedFaves = () => {
   const isAuthenticated = useSelector((state) => state.auth.token !== null);
   const [menu, setMenu] = useState(false);
+  const [focus, setFocus] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const ref = useRef();
+  useOnClickOutside(ref, () => {
+    setMenu(false);
+    setFocus(false);
+  });
+  useEffect(() => {
+    if (windowWidth > 861) {
+      setFocus(false);
+    }
+    if (windowWidth <= 861 && menu) {
+      setFocus(true);
+    }
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowWidth, menu]);
   return isAuthenticated ? (
     <>
+      {focus === true ? <div className="focus"></div> : null}
       <Header />
-      <div className="layout">
+      <div className={menu ? "layout scroll-hidden" : "layout"} ref={ref}>
         <SideBar desktop={true} />
         <AllConferences data={"favourites"} />
         {menu ? <SideBar mobile={true} /> : null}
       </div>
-      <FloatingMenu menu={menu} setMenu={setMenu} />
+      <FloatingMenu
+        menu={menu}
+        setMenu={setMenu}
+        focus={focus}
+        setFocus={setFocus}
+      />
       {/* <Footer /> */}
     </>
   ) : (
