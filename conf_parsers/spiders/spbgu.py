63d8735cf091c44d8f80f357758b7dbc6b6aae05
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-09-05 16:44:41 +0300

    grants: spbgu

--- conf_parsers/spiders/spbgu.py
+++ conf_parsers/spiders/spbgu.py
@@ -1,6 +1,6 @@
 from scrapy.spiders import Rule, CrawlSpider
 from scrapy.linkextractors import LinkExtractor
-from ..items import ConferenceItem, ConferenceLoader
+from ..items import ConferenceItem, ConferenceLoader, GrantItem
 from ..parsing import default_parser_xpath, get_dates
 
 
@@ -23,5 +23,25 @@ class SbpguSpider(CrawlSpider):
         new_item.add_xpath('conf_address', "string(//div[@class='event-details__place'])")
 
         for line in response.xpath("//div[@class='post']//*[self::p or self::ul]"):
+            new_item = default_parser_xpath(line, new_item)
+        yield new_item.load_item()
+
+
+class SbpguGrantSpider(CrawlSpider):
+    name = "grant_spbgu"
+    un_name = 'Санкт-Петербургский государственный университет'
+    allowed_domains = ["spbu.ru"]
+    start_urls = ["https://nauka.spbu.ru/nauchnye-granty-i-konkursy.html"]
+    rules = (
+        Rule(LinkExtractor(restrict_css='h3.catItemTitle', restrict_text='(?i)конкурс'),
+             callback="parse_items", follow=False),
+    )
+
+    def parse_items(self, response):
+        new_item = ConferenceLoader(item=GrantItem(), selector=response)
+
+        new_item.add_value('source_href', response.url)
+        new_item.add_css('title', "h2.itemTitle::text")
+        for line in response.xpath("//div[@class='itemFullText']//*[self::p or self::ul]"):
             new_item = default_parser_xpath(line, new_item)
         yield new_item.load_item()
