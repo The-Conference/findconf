c1adc9a2a94fd3512b33b1db863fed744b51282f
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-09-06 16:59:52 +0300

    grant_tpu

--- conf_parsers/spiders/tpu.py
+++ conf_parsers/spiders/tpu.py
@@ -1,8 +1,8 @@
 from scrapy.spiders import Rule, CrawlSpider
 from scrapy.linkextractors import LinkExtractor
 
-from ..items import ConferenceItem, ConferenceLoader
-from ..parsing import get_dates, parse_conf
+from ..items import ConferenceItem, ConferenceLoader, GrantItem
+from ..parsing import get_dates, parse_conf, default_parser_xpath
 
 
 class TpuSpider(CrawlSpider):
@@ -41,3 +41,25 @@ class TpuSpider(CrawlSpider):
                 for i in contacts[1:]:
                     new_item.add_value('contacts', i.get())
                 yield new_item.load_item()
+
+
+class TpuGrantSpider(CrawlSpider):
+    name = "grant_tpu"
+    un_name = 'Томский технологический институт'
+    allowed_domains = ["tpu.ru"]
+    start_urls = ["https://portal.tpu.ru/departments/otdel/ontp"]
+    rules = (
+        Rule(LinkExtractor(restrict_css='div.newslink', restrict_text='(?i)конкурс|грант'),
+             callback="parse_items", follow=False),
+    )
+
+    def parse_items(self, response):
+        new_item = ConferenceLoader(item=GrantItem(), selector=response)
+
+        new_item.add_value('source_href', response.url)
+        new_item.add_css('title', "div.newshead::text")
+
+        for line in response.xpath("//div[@class='newsbody']/*[self::p or self:: ul]"):
+            new_item = default_parser_xpath(line, new_item)
+
+        yield new_item.load_item()
